0ab85430adb00c50181e7521eb069b100c4771e7
James Hart <james.elliot.hart@gmail.com>
2025-09-24 17:28:15 -0400

    update pdf generation options as behavior has changed

--- convert.sh
+++ convert.sh
@@ -2,6 +2,13 @@ filename="JamesHartResume_$(date +%Y%m%d).pdf"
 tempfile="temp.md"
 
 sed "s/{{FILENAME}}/$filename/g" JamesHartResume.md > $tempfile
-pandoc $tempfile --pdf-engine=wkhtmltopdf -o "pdf/$filename" -V margin-top=10 -V margin-left=20 -V margin-right=20 -V margin-bottom=10
+pandoc $tempfile --pdf-engine=wkhtmltopdf \
+    --pdf-engine-opt=--margin-top --pdf-engine-opt=10 \
+    --pdf-engine-opt=--margin-left --pdf-engine-opt=20 \
+    --pdf-engine-opt=--margin-right --pdf-engine-opt=20 \
+    --pdf-engine-opt=--margin-bottom --pdf-engine-opt=10 \
+    --pdf-engine-opt=--page-width --pdf-engine-opt=210 \
+    --pdf-engine-opt=--page-height --pdf-engine-opt=297 \
+    -o "pdf/$filename"
 
 rm $tempfile
